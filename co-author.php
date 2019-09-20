14182034bffdada2738331c6a89e1ad85851f4d8
kachan64 <48898999+kachan64@users.noreply.github.com>
2019-09-20 22:41:59 +1000

    Update co-author.php

--- co-author.php
+++ co-author.php
@@ -1,13 +1,14 @@
 <?php
 //** Custom code to separate multiple co-authors based on http://www.orochilabs.com/2015/03/display-a-comma-separated-list-of-categories-for-a-post-in-wordpress/
-					global $post;
-					$author_list = '';
-					$author_id=$post->post_author;
-					foreach( get_coauthors(',') as $coauthor ) {
-						$author_link = home_url( '/members/' . $coauthor->user_nicename  . '/audios/' );
-						if(!empty($author_list)) {
-							$author_list .= ', ';
-						}
-						$author_list .= '<a href="'.$author_link.'">'.$coauthor->display_name.'</a>';
-					}
+global $post;
+$author_list = '';
+$author_id=$post->post_author;
+foreach( get_coauthors(',') as $coauthor ) {
+	$author_link = home_url( '/members/' . $coauthor->user_nicename  . '/posts/' );
+	if(!empty($author_list)) {
+		$author_list .= ', ';
+	}
+	$author_list .= '<a href="'.author_link.'">'.$coauthor->display_name.'</a>';
+}
+echo $author_list;
 ?>
